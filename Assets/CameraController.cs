e8825744ae26931851dda7c9cc16ebbdf46a622d
bruce-x-wu <wu.bruce.x@gmail.com>
2023-06-17 22:56:17 -0700

    feat: add camera controls

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameController gameController;
-    
+    public float startingRadiusMultiplier = 2f;
+    public float radiusScaleSpeed = 1f;
+    public float thetaScaleSpeed = 1f;
+    public float phiScaleSpeed = 1f;
+    private SphericalTransform sphericalTransform;
+    private SphericalTransformClamp sphericalTransformClamp;
+    private Vector3 cameraOrigin;
+
     void Start()
     {
-        SphericalTransform cameraSphericalTransform = GetComponent<SphericalTransform>();
-        Vector3 cameraOrigin = new Vector3(gameController.BOARD_X / 2, 0, gameController.BOARD_Y / 2);
+        sphericalTransform = GetComponent<SphericalTransform>();
+        sphericalTransformClamp = GetComponent<SphericalTransformClamp>();
+        cameraOrigin = new Vector3(gameController.BOARD_X / 2, 0, gameController.BOARD_Y / 2);
+        
         float radius = (new Vector3(gameController.BOARD_X / 2, gameController.BOARD_Z, gameController.BOARD_Y / 2)).magnitude;
 
-        cameraSphericalTransform.origin = cameraOrigin;
-        cameraSphericalTransform.radius = 2f * radius;
-        cameraSphericalTransform.theta = Mathf.Atan(cameraOrigin.z / cameraOrigin.x);
-        cameraSphericalTransform.phi = Mathf.Acos(gameController.BOARD_Z / radius);
+        sphericalTransform.origin = cameraOrigin;
+        sphericalTransform.radius = startingRadiusMultiplier * radius;
+        sphericalTransform.theta = Mathf.Atan(cameraOrigin.z / cameraOrigin.x);
+        sphericalTransform.phi = Mathf.Acos(gameController.BOARD_Z / radius);
 
-        GetComponent<SphericalTransformClamp>().radiusClamp = new Vector2(0, 5f * radius);
+        sphericalTransformClamp.radiusClamp = new Vector2(0, startingRadiusMultiplier * startingRadiusMultiplier * radius);
 
         transform.rotation = Quaternion.LookRotation(cameraOrigin - transform.position, Vector3.up); 
     }
@@ -26,5 +35,27 @@ public class CameraController : MonoBehaviour
     {
         Vector3 lookDirection = new Vector3(gameController.BOARD_X / 2, 0, gameController.BOARD_Y / 2) - transform.position;
         transform.rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+
+        sphericalTransform.radius -= Input.mouseScrollDelta.y * radiusScaleSpeed;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            sphericalTransform.theta -= Time.deltaTime * thetaScaleSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            sphericalTransform.theta += Time.deltaTime * thetaScaleSpeed;
+        }
+    
+        if (Input.GetKey(KeyCode.W))
+        {
+            sphericalTransform.phi -= Time.deltaTime * phiScaleSpeed;
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            sphericalTransform.phi += Time.deltaTime * phiScaleSpeed;
+        }
     }
 }
